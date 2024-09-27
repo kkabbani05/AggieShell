7b3c7382c658841b8796880af20814d69726d4b7
kkabbani05 <knkabbani@gmail.com>
2024-09-27 10:34:15 -0500

    Passsing first 6 test

--- shell.cpp
+++ shell.cpp
@@ -2,45 +2,69 @@
 
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
 #include <vector>
 #include <string>
-
+#include <cstring>
+#include <fcntl.h>
 #include "Tokenizer.h"
 
 // all the basic colours for a shell prompt
-#define RED     "\033[1;31m"
-#define GREEN	"\033[1;32m"
-#define YELLOW  "\033[1;33m"
-#define BLUE	"\033[1;34m"
-#define WHITE	"\033[1;37m"
-#define NC      "\033[0m"
+#define RED "\033[1;31m"
+#define GREEN "\033[1;32m"
+#define YELLOW "\033[1;33m"
+#define BLUE "\033[1;34m"
+#define WHITE "\033[1;37m"
+#define NC "\033[0m"
 
 using namespace std;
 
-int main () {
-    for (;;) {
+int main()
+{
+    for (;;)
+    {
         // need date/time, username, and absolute path to current dir
-        cout << YELLOW << "Shell$" << NC << " ";
-        
+        auto username = getenv("USER");
+
+        time_t current_time;
+        time(&current_time);
+        string cur_time = ctime(&current_time);
+
+        const size_t dic_size = 1024;
+        char current_directory[dic_size];
+
+        char prev_directory[dic_size];
+        getcwd(prev_directory, dic_size);
+        getcwd(current_directory, dic_size);
+
+        // use compy to keep previous
+
+        cout << cur_time.substr(4, 16) << username << ":" << current_directory;
+
+        cout << YELLOW << "$" << NC << " ";
+
         // get user inputted command
         string input;
         getline(cin, input);
 
-        if (input == "exit") {  // print exit message and break out of infinite loop
-            cout << RED << "Now exiting shell..." << endl << "Goodbye" << NC << endl;
+        if (input == "exit")
+        { // print exit message and break out of infinite loop
+            cout << RED << "Now exiting shell..." << endl
+                 << "Goodbye" << NC << endl;
             break;
         }
 
         // get tokenized commands from user input
         Tokenizer tknr(input);
-        if (tknr.hasError()) {  // continue to next prompt if input had an error
+        if (tknr.hasError())
+        { // continue to next prompt if input had an error
             continue;
         }
 
-        // // print out every command token-by-token on individual lines
-        // // prints to cerr to avoid influencing autograder
+        // print out every command token-by-token on individual lines
+        // prints to cerr to avoid influencing autograder
         // for (auto cmd : tknr.commands) {
         //     for (auto str : cmd->args) {
         //         cerr << "|" << str << "| ";
@@ -55,26 +79,98 @@ int main () {
         // }
 
         // fork to create child
-        pid_t pid = fork();
-        if (pid < 0) {  // error check
-            perror("fork");
-            exit(2);
-        }
 
-        if (pid == 0) {  // if child, exec to run command
-            // run single commands with no arguments
-            char* args[] = {(char*) tknr.commands.at(0)->args.at(0).c_str(), nullptr};
+        for (size_t i = 0; i < tknr.commands.size(); i++)
+        {
+            // create pipe
+            // int c2pfd[2]; /* child-to-parent */
+            // char buffer[10];
+            // memset(buffer, 0, sizeof(buffer));
+            // if ((pipe(c2pfd) < 0))
+            // {
+            //     printf("ERROR: Failed to open pipe\n");
+            //     exit(1);
+            // }
 
-            if (execvp(args[0], args) < 0) {  // error check
-                perror("execvp");
+            // IF The First Command is cd we do not want to use fork/exec
+
+            // if(tknr.commands.at(0)->args.at(0) == "cd"){
+            //     string new_dir = tknr.commands.at(0)->args.at(1);
+            //     if (new_dir == "-"){
+            //         cout << "in here" << endl;
+            //     }
+            //     else{
+            //         strcpy(prev_directory, current_directory);
+            //         strcpy(current_directory, new_dir.c_str()); 
+            //         if(chdir((char *)current_directory) != 0){
+            //             cerr << "Directory not found" << endl; //what should this output be
+            //         }
+            //     }
+            //     continue;
+            // }
+
+            pid_t pid = fork();
+            if (pid < 0)
+            { // error check
+                perror("fork");
                 exit(2);
             }
-        }
-        else {  // if parent, wait for child to finish
-            int status = 0;
-            waitpid(pid, &status, 0);
-            if (status > 1) {  // exit if child didn't exec properly
-                exit(status);
+
+            if (pid == 0)
+            { // if child, exec to run command
+                // run single commands with no arguments
+                // Loop through each of the command
+
+                if(tknr.commands.at(0)->hasInput()){
+                    int fd = open(tknr.commands.at(0)->in_file.c_str(), O_RDONLY, 0600);
+                    dup2(fd, 0);
+                }
+                if(tknr.commands.at(0)->hasOutput()){
+                    int fd = open(tknr.commands.at(0)->out_file.c_str(), O_CREAT | O_WRONLY | O_TRUNC, 0600);
+                    dup2(fd, 1);
+                }
+
+                if (i == tknr.commands.size() - 1)
+                {
+                    char **args = new char *[tknr.commands.at(i)->args.size()];
+                    for (size_t j = 0; j < tknr.commands.at(i)->args.size(); j++)
+                    {
+                        args[j] = (char *)tknr.commands.at(i)->args.at(j).c_str();
+                    }
+
+                    //
+
+                    if (execvp(args[0], args) < 0)
+                    { // error check
+                        perror("execvp");
+                        exit(2);
+                    }
+                    delete[] args;
+                }
+                else
+                {
+                    // standard out to point write end
+                    // dup2(c2pfd[1], 1);
+                    // /* Child closes read end of c2p */
+                    // close(c2pfd[0]);
+
+
+                }
+            }
+            else
+            { // if parent, wait for child to finish
+
+                // close write end
+                // redirect to the read end
+                // dup2(c2pfd[0], 0);
+                // close(c2pfd[1]);
+
+                int status = 0;
+                waitpid(pid, &status, 0);
+                if (status > 1)
+                { // exit if child didn't exec properly
+                    exit(status);
+                }
             }
         }
     }
